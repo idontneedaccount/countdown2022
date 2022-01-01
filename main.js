aea26228f925eb5f28aa3a44887ad9dd258fc5d6
idontneedaccount <83487861+idontneedaccount@users.noreply.github.com>
2022-01-01 19:54:14 +0700

    Update main.js

--- main.js
+++ main.js
@@ -7,8 +7,8 @@ $(document).ready(
 
 function start() {
   document.getElementById('title').innerText='NEW YEAR COUNT DOWN'
-  var countDownDate = new Date("2022  Jan 01 00:00:00").getTime();
-  var sayTime = new Date("2021 Dec 31 23:59:49").getTime();
+  var countDownDate = new Date("2022  Feb 01 00:00:00").getTime();
+  var sayTime = new Date("2022 Jan 31 23:59:49").getTime();
 
   // Update the count down every 1 second
   var x = setInterval(function () {
